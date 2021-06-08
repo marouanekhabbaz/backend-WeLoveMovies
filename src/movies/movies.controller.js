fc72475ba6c70d730a039a7e13d3b7f01b98b3ef
marouane khabbaz <77408531+marouanekhabbaz@users.noreply.github.com>
2021-06-07 21:54:03 -0400

    Update movies.controller.js

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,46 +1,46 @@
 const movies = require("./movies.service")
 
 async function readTheaters  (req, res , next){
-    const {movieId} = req.params 
- let data = await movies.readTh(movieId)
-res.json({ data })
+    const {movieId} = req.params ;
+ let data = await movies.readTh(movieId);
+res.json({ data });
 }
 
 async function readTReviews (req, res , next){
-    const {movieId} = req.params 
- let data = await movies.readRev(movieId)
-res.json({ data })
+    const {movieId} = req.params ;
+ let data = await movies.readRev(movieId);
+res.json({ data });
 }
 
 
 async function list (req, res , next){
-    const  {is_showing} = req.query
- let data = await movies.list(is_showing)
+    const  {is_showing} = req.query;
+ let data = await movies.list(is_showing);
  
-res.json({ data })
+res.json({ data });
 }
 
 async function movieExist(req , res , next){
-    const {movieId} = req.params
-    let movie = await movies.readData(movieId)
+    const {movieId} = req.params;
+    let movie = await movies.readData(movieId);
     if(movie){
-        next()
+        next();
     } else{
         next({
             status:404,
             message:"Movie cannot be found."
-        })
+        });
     }
 }
 
 async function read(req, res , next){
     try{
-    const {movieId} = req.params
-let data = await movies.readData(movieId)
-res.json({ data })
+    const {movieId} = req.params;
+let data = await movies.readData(movieId);
+res.json({ data });
     }
     catch(err){
-        console.log(err)
+        console.log(err);
     }
 }
 
