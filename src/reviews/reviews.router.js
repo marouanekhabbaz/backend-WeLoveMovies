71ed7854404e3328241a33b72c43c34476c8e022
marouane khabbaz <77408531+marouanekhabbaz@users.noreply.github.com>
2021-06-07 21:56:18 -0400

    Update reviews.router.js

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const controller = require("./reviews.controller");
-const methodNotAllowed= require("../error/methodNotAllowed")
-router.route('/:reviewId').delete(controller.destroy).put(controller.update).all(methodNotAllowed)
+const methodNotAllowed= require("../error/methodNotAllowed");
+router.route('/:reviewId').delete(controller.destroy).put(controller.update).all(methodNotAllowed);
 
 
 module.exports= router
