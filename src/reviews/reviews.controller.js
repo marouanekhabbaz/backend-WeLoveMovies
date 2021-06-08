76c24ff41d56f8d858ea4616dff12f3ad6d00010
marouane khabbaz <77408531+marouanekhabbaz@users.noreply.github.com>
2021-06-07 21:55:59 -0400

    Update reviews.controller.js

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,37 +1,37 @@
 const reviews = require("../reviews/reviews.service")
  
 async function exist(req , res , next){
-  const   {reviewId} = req.params
-  let data = await reviews.list(reviewId)
+  const   {reviewId} = req.params;
+  let data = await reviews.list(reviewId);
   
 if(data){
-    next()
+    next();
 } else{
     next({
         status:404,
         message:"Review cannot be found."
-    })
+    });
 }
 }
 
 async function destroy(req , res , next){
-    const   {reviewId} = req.params
-    let data = await reviews.delete(reviewId)
-    res.sendStatus(204)
+    const   {reviewId} = req.params;
+    let data = await reviews.delete(reviewId);
+    res.sendStatus(204);
 }
 
 async function update(req , res , next){
-    const   {reviewId} = req.params
+    const   {reviewId} = req.params;
     const review = {
         ...req.body.data,
         review_id: reviewId
-    }
-let updated = await  reviews.update(review)
-let data = await reviews.readRev(reviewId)
-res.json({data})
+    };
+let updated = await  reviews.update(review);
+let data = await reviews.readRev(reviewId);
+res.json({data});
 }
 
 module.exports= {
     destroy:[exist , destroy],
     update:[exist, update]
-}
+};
