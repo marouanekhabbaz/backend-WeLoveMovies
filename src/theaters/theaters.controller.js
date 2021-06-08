9eaa93953d9000831bd38256669a0cbf7d43f33d
marouane khabbaz <77408531+marouanekhabbaz@users.noreply.github.com>
2021-06-07 21:57:23 -0400

    Update theaters.controller.js

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,10 +1,10 @@
-const theaters = require("./theaters.service")
+const theaters = require("./theaters.service");
 
 async function list (req , res , next){
-    const data = await theaters.list()
-    res.json({data})
+    const data = await theaters.list();
+    res.json({data});
 }
 
 module.exports = {
     list
-}
+};
